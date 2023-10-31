fecbc457471b593a56b8a4d59778d11a81888603
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-31 10:59:23 -0400

    add logout route for auth blueprint

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -53,8 +53,24 @@ def login():
         
         if error is None:
             session.clear()
-            session['user.id'] = user['id']
+            session['user_id'] = user['id']
             return redirect(url_for('index'))
         
         flash(error)
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
